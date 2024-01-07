a4f63dd717d7bd380a05a0c92bfdad9412fba591
pprobst <pprobst@insiberia.net>
2024-01-07 16:33:47 -0300

    no_timestamps=True by default

--- whisper.cpp
+++ whisper.cpp
@@ -4318,7 +4318,7 @@ struct whisper_full_params whisper_full_default_params(enum whisper_sampling_str
 
         /*.translate         =*/ false,
         /*.no_context        =*/ true,
-        /*.no_timestamps     =*/ false,
+        /*.no_timestamps     =*/ true,
         /*.single_segment    =*/ false,
         /*.print_special     =*/ false,
         /*.print_progress    =*/ true,
